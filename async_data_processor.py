90a698de974c97982c060845ab2dc81519bb6f6f
tbergkvist <bergkvist.teo@protonmail.com>
2024-04-19 16:11:36 +0200

    Connect this code to NanoVNAHeadless.

--- async_data_processor.py
+++ async_data_processor.py
@@ -6,7 +6,9 @@ from signal_processing import (
 from simulate_data import (
     SimulateData,
 )
-from nanovna_saver_headless.src.NanoVNASaverHeadless import NanoVNASaverHeadless
+from nanovna_data import (
+    NanoVNAData
+)
 
 # File used for calibratin the vna. 
 CALIBRATION_FILE = (
@@ -15,17 +17,15 @@ CALIBRATION_FILE = (
 
 
 class AsyncDataProcessor:
-    def __init__(self, simulate=False, verbose=False):
-        self.simulate = simulate  # True if we are simulating data, false if we are streaming from the nanoVNA
+    def __init__(self, datafile=False, verbose=False):
+        self.simulate = not datafile  # True if we are simulating data, false if we are streaming from the nanoVNA
         self.verbose = verbose
         self.data_queue = asyncio.Queue()  # Queue for inter-class communication
         if self.simulate:  # Runs on simulated data.
             self.data_source = SimulateData(self.data_queue, verbose=self.verbose)
         else:  # Runs on streamed data from the vna.
             # TODO: Put nanoVNA data into a async Queue that is refered to as self.data_source. 
-            self.data_source = NanoVNASaverHeadless(vna_index=0, verbose=self.verbose)
-            self.data_source.calibrate(None, CALIBRATION_FILE)  # Calibrates the vna
-            self.data_source.set_sweep(2.9e9, 3.1e9, 1, 101)  # Sets the sweep.
+            self.data_source = NanoVNAData(self.data_queue, self.verbose)
 
         self.signal_processing = SignalProcessing(
             self.data_queue, verbose=self.verbose
@@ -33,23 +33,14 @@ class AsyncDataProcessor:
         self.running = True
 
     async def run(self):
-        if self.simulate:
-            start_buffer_data = asyncio.create_task(
-                self.data_source.generate_start_data()
-            )  # Reads a few data points before the processing starts, to avoid empty queue
-            producer = asyncio.create_task(
-                self.data_source.generate_data_continuously()
-            )
-        else:
-            # TODO: Make sure that 'self.data_source' is a async Queue with functions for handeling continuous data streaming. 
-            start_buffer_data = ...
-            producer = ...
+        producer = asyncio.create_task(
+            self.data_source.generate_data_continuously(self.datafile)
+        )
 
         consumer = asyncio.create_task(
             self.signal_processing.process_data_continuously()
         )
         try:
-            await asyncio.gather(start_buffer_data)
             await asyncio.gather(producer, consumer)
         except asyncio.CancelledError:
             pass  # Handle cleanup if necessary
--- nanovna_data.py
+++ nanovna_data.py
@@ -1,32 +1,27 @@
 import asyncio
-import random
+from nanovna_saver_headless.src.NanoVNASaverHeadless import NanoVNASaverHeadless
 
 
-class SimulateData:
+class NanoVNAData:
     def __init__(self, data_queue, verbose):
         self.data_queue = data_queue
         self.verbose = verbose
+        self.vna = NanoVNASaverHeadless(0, self.verbose)
 
-    async def generate_start_data(self):
-        for i in range(5):
-            s11_data = self.generate_complex_data()
-            s21_data = self.generate_complex_data()
-            await self.data_queue.put((s11_data, s21_data))
-
-    async def generate_data_continuously(self, placeholder=None):
-        while True:
-            s11_data = self.generate_complex_data()
-            s21_data = self.generate_complex_data()
+    async def generate_data_continuously(self, datafile=False):
+        if self.vna.playback_mode and datafile:
+            print("No VNA is connected, a playback file must be used. Quitting.")
+            return
+        data = self.vna.stream_data(datafile)
+        for sweep in data:
+            s11_re, s11_im, s21_re, s21_im, freq = sweep
+            s11_data = complex(s11_re, s11_im)
+            s21_data = complex(s21_re, s21_im)
             await self.data_queue.put((s11_data, s21_data))
             if self.verbose:
                 print(f"Generated data: S11={s11_data}, S21={s21_data}")
                 print(f"Queue length: {self.data_queue.qsize()}")
             await asyncio.sleep(2)  # Added sleep to simulate time delay
-
-    def generate_complex_data(self):
-        real_part = random.uniform(-1, 1)
-        imag_part = random.uniform(-1, 1)
-        return complex(real_part, imag_part)
 
     async def consume_data(self):
         while True:
