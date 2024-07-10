822396323d06d97713e5e6432af90cfad430a2d4
tbergkvist <69448973+tbergkvist@users.noreply.github.com>
2024-07-10 21:18:34 +0200

    Feature/improve UI (#4)
    * Add log box.
    
    * Fixed axises.
    
    * Add update car values functionality.
    
    * Add reboot and shutdown button functionality.
    
    * Update requirements.
    
    * Add reference value taking.
    
    * Add calibration file change functionality.
    
    * Update setup.py file.
    
    * Update path in setup.py

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from flask import Flask, render_template, jsonify, request
 from bt_car_control import BTSender
 import time
 import asyncio
+from flask_socketio import SocketIO, emit
+import subprocess
 
-DATA_FILE = False#"../save_plate.csv"
+DATA_FILE = "../save_plate.csv"
 CALIBRATION_FILE = "../cal0514.cal"
-VERBOSE = False
+VERBOSE = True
 PROCESS_SLEEP_TIME = 0.0001
 CAR_DEVICE_NAME = "BT05-BLE"  # Replace with your car's Bluetooth device name
 
 # Flask application setup
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 latest_data = {"angle": 1, "throttle": 1}  # Initialize with default values
 received_data = {}  # Dictionary to store received data
@@ -39,23 +42,26 @@ def receive_data():
     global received_data
     data = request.get_json()
     received_data = data
-    print("Received data from web:", received_data)
     return jsonify({"status": "success", "data": received_data})
 
 def run_flask():
-    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=False, use_reloader=False)
 
 # NanoVNA Setup
-data_source = pynanovna.NanoVNAWorker(verbose=VERBOSE)
-data_source.calibrate(load_file=CALIBRATION_FILE)  # This needs to be done through a terminal atm.
-data_source.set_sweep(2.9e9, 3.1e9, 1, 101)
-data_stream = data_source.stream_data(DATA_FILE)
+def setup_nanovna(verbose, calibration_file, data_file, process_sleep_time):
+    data_source = pynanovna.NanoVNAWorker(verbose=verbose)
+    data_source.calibrate(load_file=calibration_file)  # This needs to be done through a terminal atm.
+    data_source.set_sweep(2.9e9, 3.1e9, 1, 101)
+    data_stream = data_source.stream_data(data_file)
 
-signal_processing = SignalProcessing(
-    data_stream,
-    process_sleep_time=PROCESS_SLEEP_TIME,
-    verbose=VERBOSE,
-)
+    signal_processing = SignalProcessing(
+        data_stream,
+        process_sleep_time=process_sleep_time,
+        verbose=verbose,
+        interactive_mode=False
+    )
+    return signal_processing
+
 
 # Start Flask in a separate thread
 flask_thread = threading.Thread(target=run_flask)
@@ -65,29 +71,105 @@ flask_thread.start()
 bt_sender = BTSender(device_name=CAR_DEVICE_NAME)
 
 async def main_loop():
-    await bt_sender.connect()
-    data_processor = signal_processing.process_data_continuously()
-    
-    for angle, throttle in data_processor:
-        send_data = {'angle': angle, 'throttle': throttle}
-        latest_data = send_data.copy()
-        try:
-            response = requests.post("http://127.0.0.1:5000/update_data", json=send_data)
-            response.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            print(f"Error sending data to server: {e}")
-        
-        # Send the data to the RC car
-        if not bt_sender.is_connected():
-            print("Car not connected!")
-            print("Trying to connect car...")
-            await bt_sender.connect()
-            time.sleep(5)
+    signal_processing = setup_nanovna(VERBOSE, CALIBRATION_FILE, DATA_FILE, PROCESS_SLEEP_TIME)
+    while True:
+        await bt_sender.connect()
+        data_processor = signal_processing.process_data_continuously()
+        update_processor = False
+
+        for angle, throttle in data_processor:
+            send_data = {'angle': angle, 'throttle': throttle}
+            latest_data = send_data.copy()
+
+            try:
+                response = requests.post("http://127.0.0.1:5000/update_data", json=send_data)
+                response.raise_for_status()
+            except requests.exceptions.RequestException as e:
+                log_message(f"Error sending data to server: {e}")
             
-        await bt_sender.update_speed(angle, throttle)
-        
-        print("Still in loop", received_data)
+            # Send the data to the RC car        
+            await bt_sender.update_speed(angle, throttle)
+
+            # Handle any received data and then reset it
+            update_processor = await handle_received_data(received_data, bt_sender, signal_processing)
+            received_data.clear()  # Reset received_data to an empty dictionary after handling
+            if update_processor:
+                break
+
+            time.sleep(1) # Uncomment this row if running from prerecorded file.
+        if update_processor:
+            setup_nanovna(VERBOSE, CALIBRATION_FILE, DATA_FILE, PROCESS_SLEEP_TIME)
+
+def log_message(message):
+    """Send log message to connected clients."""
+    socketio.emit('log_message', {'message': message})
+    print(message)
+
+async def handle_received_data(received_data, bt_sender, signal_processing):
+    if received_data == {}:
+        return False
+
+    if received_data["button"] == "updateAngleButton":
+        try:
+            min_threshold = int(received_data["angleThresholdMin"])
+            max_threshold = int(received_data["angleThresholdMax"])
+            max_angle = int(received_data["angleMax"])
+            if min_threshold != 0:
+                bt_sender.angle_min_threshold = min_threshold
+            if max_threshold != 0:
+                bt_sender.angle_max_threshold = max_threshold
+            if max_angle != 0:
+                bt_sender.max_steering_angle = max_angle
+            log_message("Updating car angle thresholds.")
+        except ValueError:
+            log_message("Could not update the car angle thresholds, are they valid?")
+    elif received_data["button"] == "updateSpeedButton":
+        try:
+            min_speed = int(received_data["speedMin"])
+            max_speed = int(received_data["speedMax"])
+            if min_threshold != 0:
+                bt_sender.min_motor_speed= min_speed
+            if max_threshold != 0:
+                bt_sender.max_motor_speed = max_speed
+            log_message("Updating car speed thresholds.")
+        except ValueError:
+            log_message("Could not update the car speed thresholds, are they valid?")
+    elif received_data["button"] == "rebootButton":
+        log_message("Rebooting the system.")
+        subprocess.run(['sudo', 'reboot', 'now'], check=True)
+    elif received_data["button"] == "shutdownButton":
+        log_message("Shutting down the system. Goodbye.")
+        subprocess.run(['sudo', 'shutdown', 'now'], check=True)
+    elif received_data["button"] == "connectCar":
+        log_message("Trying to connect to the car.")
+        if not bt_sender.is_connected():
+            log_message("Car not connected!")
+            log_message("Trying to connect car...")
+            await bt_sender.connect()
+    elif received_data["button"] == "refMeasure0":
+        log_message("Running reference measure CLEAR.")
+        signal_processing.reference_step0()
+    elif received_data["button"] == "refMeasureSetup":
+        log_message("Running reference setup.")
+        signal_processing.setup(False)
         time.sleep(1)
+        return True #  Makes the for loop restart with a new processor.
+    elif "refMeasure" in received_data["button"]:
+        stepno = int(received_data["button"][-1])
+        log_message(f"Running reference measure step {stepno}.")
+        signal_processing.reference_step_n(stepno)
+    elif received_data["button"] == "updateCalibrationFile":
+        global CALIBRATION_FILE
+        log_message(f"Changing calibration file. Old file: {CALIBRATION_FILE}")
+        CALIBRATION_FILE = received_data["calibrationFile"]
+        log_message(f"Calibration file changed. New file: {CALIBRATION_FILE}")
+        time.sleep(1)
+        return False
+
+
+    time.sleep(1)
+    log_message("Done.")
+    return False
 
 # Run the main loop
 asyncio.run(main_loop())
