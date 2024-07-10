d51174881f8e98065827531429435993751f449f
tbergkvist <bergkvist.teo@protonmail.com>
2024-07-10 21:14:30 +0200

    Update setup.py file.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 import os
 
-
+# Install required packages from requirements.txt
 subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 
 def create_service_file():
@@ -27,11 +27,24 @@ def create_service_file():
 
 def setup_service():
     create_service_file()
+    subprocess.check_call(["sudo", "systemctl", "daemon-reload"])
     subprocess.check_call(["sudo", "systemctl", "enable", "main_script.service"])
     subprocess.check_call(["sudo", "systemctl", "start", "main_script.service"])
+
+def add_startup_command():
+    bashrc_path = os.path.expanduser("~/.bashrc")
+    startup_command = "sudo systemctl start main_script.service\n"
+    
+    with open(bashrc_path, "r") as bashrc:
+        lines = bashrc.readlines()
+    
+    if startup_command not in lines:
+        with open(bashrc_path, "a") as bashrc:
+            bashrc.write(startup_command)
 
 # Run the setup
 if os.geteuid() != 0:
     print("Please run the script as root.")
 else:
     setup_service()
+    add_startup_command()
